016bb58960a44ab50dd412648ccc0991745df946
vmxpose <98051561+vmxpose@users.noreply.github.com>
2025-10-09 06:36:15 +0000

    Add tab button styles and indicator configuration

--- libraries/styles.lua
+++ libraries/styles.lua
@@ -74,6 +74,72 @@ styles.default = {
             horizontalAlignment = Enum.HorizontalAlignment.Left,
             verticalAlignment = Enum.VerticalAlignment.Center
         }
+    },
+    tab_button = {
+        anchorPoint = Vector2.new(0.5, 0.5),
+        size = UDim2.new(0, 75, 0, 30),
+        textFont = Enum.Font.RobotoMono,
+        textSize = 14,
+        textXAlignment = Enum.TextXAlignment.Center,
+        textYAlignment = Enum.TextYAlignment.Center,
+        off = {
+            textColor = Color3.fromRGB(150,150,150),
+            gradient = {
+                ColorSequence = ColorSequence.new{
+                    ColorSequenceKeypoint.new(0, Color3.fromRGB(150,150,150)),
+                    ColorSequenceKeypoint.new(1.0, Color3.fromRGB(255,255,255))
+                },
+                Rotation = 45
+            },
+            padding = {
+                bottom = UDim.new(0, 0),
+                left = UDim.new(0, 10),
+                right = UDim.new(0, 10),
+                top = UDim.new(0, 0)
+            }
+        },
+        on = {
+            textColor = Color3.fromRGB(255,255,255),
+            gradient = {
+                ColorSequence = ColorSequence.new{
+                    ColorSequenceKeypoint.new(0, Color3.fromRGB(255,255,255)),
+                    ColorSequenceKeypoint.new(1.0, Color3.fromRGB(150,150,150))
+                },
+                Rotation = 90
+            },
+        },
+        hover = {
+            textColor = Color3.fromRGB(255,255,255),
+            gradient = {
+                ColorSequence = ColorSequence.new{
+                    ColorSequenceKeypoint.new(0, Color3.fromRGB(200,200,200)),
+                    ColorSequenceKeypoint.new(1.0, Color3.fromRGB(255,255,255))
+                },
+                Rotation = 90
+            },
+            padding = {
+                bottom = UDim.new(0, 5),
+                left = UDim.new(0, 10),
+                right = UDim.new(0, 10),
+                top = UDim.new(0, 0)
+            }
+        }
+    },
+    indicator = {
+        background = Color3.fromRGB(150,0,0),
+        anchorPoint = Vector2.new(0.5,0.5),
+        size = UDim2.new(1, 0, 0, 2),
+        position = UDim2.new(0.5, 0, 1, 0),
+        automaticSize = Enum.AutomaticSize.X,
+        gradient = {
+            ColorSequence = ColorSequence.new{
+                ColorSequenceKeypoint.new(0, Color3.fromRGB(75,75,75)),
+                ColorSequenceKeypoint.new(0.25, Color3.fromRGB(150,150,150)),
+                ColorSequenceKeypoint.new(0.75, Color3.fromRGB(150,150,150)),
+                ColorSequenceKeypoint.new(1.0, Color3.fromRGB(75,75,75))
+            },
+            Rotation = 0
+        },
     }
 }
 
